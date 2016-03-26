8c7d0f9e1f8dd2647f77e7e58af4384f2f27c6ec
Jesse <jesse.chin@stonybrook.edu>
2016-03-26 17:12:19 -0400

    Publisher option
    Search for a specific publisher
    Limit option can be turned off using argument value 'none', no quotes

--- comicSales.pl
+++ comicSales.pl
@@ -13,6 +13,8 @@ use ComicIssue;
 my $input_url = "";
 #file flag
 my $input_file = "";
+#publisher to search for
+my $publisher_name = "";
 #help flag
 my $help = "";
 
@@ -21,13 +23,16 @@ my $max_limit = 10;
 
 #replace with Pod::Usuage
 my $help_string = "Usuage: perl comicSales.pl [switches] [arguments]
--d or --date	specify a month and year in mm/yyyy to get information from
--f or --file	specify a html file to get information from
--l or --limit	specify a limit of how many issues to show, default 10
--h or --help	this help menu";
+-d or --date		specify a month and year in mm/yyyy to get information from
+-f or --file		specify a html file to get information from
+-l or --limit		specify a limit of how many issues to show only, 
+					default 10, use none to show all
+					does not affect actual number of issues in list, use instead --hardlimit
+-p or --publisher	specify to look for a specific publisher, case insensitive
+-h or --help		this help menu";
 
-GetOptions("d|date=s" => \$input_url, "f|file=s" => \$input_file, 
-			"l|limit=i" => \&handle_limit, "h|help|?" => \$help);
+GetOptions("d|date=s" => \$input_url, "f|file=s" => \$input_file, "p|publisher=s" => \$publisher_name,
+			"l|limit=s" => \&handle_limit, "h|help|?" => \$help);
 
 #help takes precedence
 if ($help) {
@@ -37,6 +42,11 @@ if ($help) {
 
 if ($input_url and $input_file) {
 	say "You are only allowed to specify either a date or an input html file.";
+	say $help_string;
+	exit(2);
+} elsif (!$input_url and !$input_file) {
+	say "You must specify either a date or an input html file.";
+	say $help_string;
 	exit(2);
 }
 
@@ -46,6 +56,7 @@ my $url = "http://www.comichron.com/monthlycomicssales/";
 #must match mm/yyyy format
 if ($input_url) {
 	if ($input_url =~ m/(\d{2})\/(\d{4})/) {
+		#url is in the form yyyy/yyyy-mm.html
 		$tree = HTML::TreeBuilder -> new_from_url($url.$2."/".$2."-".$1.".html");
 	} else {
 		say $help_string;
@@ -74,17 +85,22 @@ for my $i (1..$#rows) {
 	$rank = $cells[0] -> as_trimmed_text();
 	#stop parsing if there is no rank
 	#indicates that there is a special note
-	# eg 04/1997
+	#eg 04/1997
 	if ($rank !~ m/\d+/) {
 		last;
 	}
+	#there isn't any special formatting for the issue name
 	$name = $cells[1] -> as_trimmed_text();
 	#may be missing issue number field in html, means there is only 5 fields, not 6
 	if (scalar @cells == 5) {
 		#remove dollar sign from price
 		$price = $cells[2] -> as_trimmed_text();
 		$price =~ s/\$//;
+		#check p flag
 		$publisher = $cells[3] -> as_trimmed_text();
+		if ($publisher_name and lc($publisher) ne lc($publisher_name)) {
+			next;
+		}
 		#remove comma from sales number
 		$sales = $cells[4] -> as_trimmed_text();
 		$sales =~ s/,//g;
@@ -105,6 +121,9 @@ for my $i (1..$#rows) {
 			$number = $1;
 		}
 		$publisher = $cells[4] -> as_trimmed_text();
+		if ($publisher_name and lc($publisher) ne lc($publisher_name)) {
+			next;
+		}
 		#remove dollar sign from price
 		$price = $cells[3] -> as_trimmed_text();
 		$price =~ s/\$//;
@@ -118,7 +137,11 @@ for my $i (1..$#rows) {
 }
 
 for my $i (0..$max_limit - 1) {
-	say $issue_list[$i] -> toString();
+	if(defined $issue_list[$i]) {
+		say $issue_list[$i] -> toString();
+	} else {
+		last;
+	}
 }
 
 say sprintf("The total sales for %d issues is %d.", scalar @issue_list, calcTotalSales(\@issue_list));
@@ -133,10 +156,14 @@ $tree = $tree -> delete();
 #
 sub handle_limit {
 	my ($opt_name, $opt_value) = @_;
-	if ($opt_value < 0) {
-		$max_limit = 10;
+	if ($opt_value =~ m/\d+/) {
+		if ($opt_value > 0) {
+			$max_limit = $opt_value;
+		} 
+	} elsif ($opt_value eq "none") {
+		$max_limit = 1000;
 	} else {
-		$max_limit = $opt_value;
+		$max_limit = 10;
 	}
 }
 
